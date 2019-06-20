b2be07066b6e53645a9d968833790b749fd82b29
marqeriq <35728577+marqeriq@users.noreply.github.com>
2019-06-20 13:51:38 +0300

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -210,7 +210,7 @@ const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
 const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
 } // parameters
 
-const char     CRYPTONOTE_NAME[]                             = "Krypto";
+const char     CRYPTONOTE_NAME[]                             = "ePesa";
 
 const uint8_t  TRANSACTION_VERSION_1                         =  1;
 const uint8_t  TRANSACTION_VERSION_2                         =  2;
@@ -265,8 +265,8 @@ const uint64_t DATABASE_READ_BUFFER_MB_DEFAULT_SIZE          = 1024;          //
 const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES               = 500;           // 500 files
 const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT     = 10;            // 10 DB threads
 
-const char     LATEST_VERSION_URL[]                          = "http://latest.turtlecoin.lol";
-const std::string LICENSE_URL                                = "https://github.com/turtlecoin/turtlecoin/blob/master/LICENSE";
+const char     LATEST_VERSION_URL[]                          = "#";
+const std::string LICENSE_URL                                = "https://github.com/marceriq/ePesa/blob/master/LICENSE";
 const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 {
     {  0xb5, 0x0c, 0x4a, 0x6c, 0xcf, 0x57, 0x52, 0x41, 0xe9, 0xf9, 0x91, 0xa4, 0xb6, 0xc1, 0x43, 0x65  }
